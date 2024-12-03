c48e50b0a16b0c8662c48564b30ff7ade1fc8bd6
barisakgis <barisakgis@gmail.com>
2024-12-03 13:23:35 +0300

    folder has been done

---
+++ client-app/src/app/models/activity.ts
@@ -0,0 +1,10 @@
+export interface Activity {
+    id: string;
+    title: string;
+    description: string;
+    category: string;
+    date: Date | null;
+    city: string;
+    venue: string;
+     
+}
